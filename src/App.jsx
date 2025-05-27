2e7711813708a6af4f6a57a61a5e86af5dadbcec
zahir ali <zahirali48734@gmail.com>
2025-05-28 03:09:48 +0500

    new file add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+// import Submit from "./submit"
+import { useTransition } from "react";
 import './App.css'
+
 function App() {
+  const [pending, startTransition] = useTransition();
+  const handleButton = ()=>{
+
+  startTransition(async ()=> {
+       await new Promise(res => setTimeout(res, 2000));
+  })
+   
+  }
   return (
-    <>
-   <h1>hello react Js</h1>
-    </>
+    <div> 
+    <button disabled={pending} onClick={handleButton}>click</button>
+    {
+      pending && <p>Loading...</p>
+    }
+    </div>
   )
 }
 export default App
---
+++ src/Submit.jsx
@@ -0,0 +1,12 @@
+// ye code parent component k andar jo form hoga us main use ho ga.
+// import { useFormStatus } from "react-dom";
+
+// function Submit(){
+//     const {pending} = useFormStatus();
+//     return(
+//         <button type="submit" disabled = {pending}>
+//             {pending ? "submitting..." : "submit"}
+//         </button>
+//     )
+// }
+// export default Submit;
